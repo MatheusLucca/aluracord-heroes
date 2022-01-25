fca364644acbbe4b173675e74ba1be1d77e2d7f2
MatheusLucca <mat.lualves@gmail.com>
2022-01-25 19:48:43 -0300

    Campo de texto dinâmico e Criação das páginas(chat e _app)

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
-
+import React from 'react';
+import { useRouter } from 'next/router'
 import appConfig from '../config.json'
 
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-  );
-}
 
 
 
@@ -65,11 +38,15 @@ function Title(props) {
 //}
 
 export default function PaginaInicial() {
-  const username = 'MatheusLucca';
+  // const username = 'MatheusLucca';
+  const [username, setUsername] = React.useState('MatheusLucca');
+  const roteamento = useRouter();
+
+
 
   return (
     <>
-      <GlobalStyle />
+
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -96,6 +73,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infoEvent) {
+              infoEvent.preventDefault();
+              console.log('a');
+              roteamento.push('/chat')
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -105,8 +87,25 @@ export default function PaginaInicial() {
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[500] }}>
               {appConfig.name}
             </Text>
+            {/* <input type="text" value={username} onChange={function (event) {
+              console.log('usuario digitou', event.target.value)
+              const valor = event.target.value;
 
+              //Trocar o valor da variavel através do react
+              setUsername(valor);
+
+            }}></input>
+          */}
             <TextField
+              value={username}
+              onChange={function (event) {
+                console.log('usuario digitou', event.target.value)
+                const valor = event.target.value;
+
+                //Trocar o valor da variavel através do react
+                setUsername(valor);
+
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
